b863ccd4e9e38cff158f8d64400175633254b1da
Miguel Fonseca <miguel.cnf@gmail.com>
2015-08-14 14:58:51 +0100

    Update version to 0.1.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'miguel.fonseca@mindera.com'
 license          'MIT'
 description      'Installs/Configures twemproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 supports 'centos'
 supports 'redhat'
